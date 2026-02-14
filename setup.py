245e4dc443e82edfa9ae77157b9752b1ab43efc1
syuririk <songjeff0@g.skku.edu>
2026-02-14 01:59:40 +0000

    1

--- setup.py
+++ setup.py
@@ -3,19 +3,17 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / "README.md").read_text(encoding="utf-8") if (here / "README.md").exists() else ""
-
 with open(here / "requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     name="get_data",
     version="0.1.0",
-    packages=find_packages(include=["funcs", "funcs.*"]),  # funcs 전체 포함
+    packages=find_packages(include=["funcs", "funcs.*"]),  # funcs 폴더 포함
     include_package_data=True,
     install_requires=requirements,
-    description="Get data from ECOS and other sources",
-    long_description=long_description,
+    description="Get data from ECOS",
+    long_description=(here / "README.md").read_text(encoding="utf-8") if (here / "README.md").exists() else "",
     long_description_content_type="text/markdown",
     python_requires=">=3.8",
 )
