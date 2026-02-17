c958972b6dfdcb93b727e1c5591327d3aa647ec0
syuririk <songjeff0@g.skku.edu>
2026-02-17 10:18:10 +0000

    0.3.1

--- processData/factorComputing.py
+++ processData/factorComputing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from . import processUtils
+from . import DataProcessUtils
 
 
 # ======================================================================
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ package_path = [n + ".*" for n in package_names]
 
 setup(
     name="utils",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(include=package_names + package_path),
     include_package_data=True,
     install_requires=requirements,
